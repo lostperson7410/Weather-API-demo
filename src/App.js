a21c425ea7ae74fa409a9ac2a5d78c2f8445f08a
lostperson7410 <55078251+lostperson7410@users.noreply.github.com>
2023-09-03 21:59:48 +0700

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import sunny from './asset/icons/sun.png';
+import cloudy from './asset/icons/cloudy.png';
+import rainy from './asset/icons/rainy.png';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+const App = () =>{
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const [data, setData] = useState()
+
+  useEffect(() => {
+    fetchAPI()
+
+  }, [])
+  
+
+  //Create AXIOS GET A WEATHER API 
+
+  //this is API Path
+  //https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+
+  const fetchAPI = async (city_name) =>{
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city_name ? city_name : "Bangkok"}&appid=cfd7024e2b79e9dc84efeff3be5aa26e`)
+    setData(res?.data)
+  }
+  
+
+  return(
+    <div className='container'>
+      <div className='main_box'>
+        <div className='inner_main_box'>
+            {
+              data?.weather?.map((item)=>(
+                <div className='col weather_box'>
+                  <div className='weather_title'>{item?.main}</div>
+                  <div className='icon_section'>
+                    <img alt='iconsimage' className='imageIcons' src={ item?.main === 'Clouds' ? cloudy : item?.main === 'Rain' ? rainy : sunny }/>
+                  </div>
+                  <div className='weather_Town'>{data?.name}</div>
+                  <div className='weather_desc'>
+                    <div className='desc_One'>{item?.description}</div>
+                    <div>
+                    feels_like : {data.main.feels_like}
+                    </div>
+                  </div>
+                </div>
+              ))
+            }
+        </div>
+      </div>
     </div>
-  );
+  )
 }
-
 export default App;
